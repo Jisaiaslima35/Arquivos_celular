6024ebb9ee9e2ae8d486b718ad8f630fa43be173
Josepisaias@hotmail.com <Josepisaias@hotmail.com>
2023-07-17 16:34:01 -0300

    todolist simples js

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello World!');
+ function salvar() {
+   var todoInput = document.getElementById('todoInput');
+   var todoList = document.getElementById('todolist');
+   var todo = document.getElementById('maim');
+
+   if (todoInput == '' || todoInput == null) {
+     alert('preencha o campo a baixo!!')
+   }
+
+   var todoDiv = document.createElement('div');
+   var todoLi = document.createElement('li'); // Alterado para <li>
+
+   todoLi.textContent = todoInput.value;
+
+   todoInput.value = '';
+   todoInput.focus();
+
+   todoList.appendChild(todoLi);
+   todoDiv.appendChild(todoList);
+   todo.appendChild(todoDiv);
+ }
